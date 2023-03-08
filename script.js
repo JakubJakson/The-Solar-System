d4b1151859c07422cbb6b6da0b6e666e1c643dc9
JakubJakson <94644257+JakubJakson@users.noreply.github.com>
2023-03-08 13:53:38 +0100

    New Commit
    Implemented  API functionality.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+// Selectors
 const main = document.querySelector(".main--container");
 const mercNav = document.getElementById("mercury");
+const names = document.querySelectorAll(".name");
+const infos = document.querySelectorAll(".planet--info");
+const specs = document.querySelectorAll(".planet--specs");
+const planetImgs = document.querySelectorAll(".planet");
 const planet = document.querySelector(".planet");
 const dots = document.querySelectorAll(".nav--dot");
 const iconRight = document.querySelector(".icon--right");
@@ -11,21 +16,70 @@ const planetInfo = document.querySelector(".planet--info");
 const planetSpecs = document.querySelector(".planet--specs");
 const planetName = document.querySelector(".name");
 const entry = document.querySelector(".entry");
+const icons = document.querySelectorAll(".icon");
 // Planets
 const planets = document.querySelectorAll(".planet--container");
+const specItems = document.querySelectorAll(".spec");
+// /////////
 
-dots.forEach((dot, index) => {
-  dot.addEventListener("click", function () {
-    if (index === 0) {
-      merc.classList.add("active--planet");
-    } else if (index === 1) {
-      venus.classList.add("active--planet");
-    } else if (index === 2) {
-      ea.classList.add("active--planet");
-    }
-  });
-});
+(async function () {
+  try {
+    const response = await fetch(
+      "https://api.le-systeme-solaire.net/rest/bodies/"
+    );
+    const apiData = await response.json();
+    const bodies = apiData.bodies;
+    const planetBodies = bodies.filter((body) => body.isPlanet);
+    const notPlanet = bodies.filter((body) => body.name === "Pluton");
+    const allPlanets = planetBodies.push(notPlanet[0]);
+    const allBodies = planetBodies.sort((a, b) => a.aphelion - b.aphelion);
+    // console.log(planetBodies);
+    console.log(allBodies);
+    planets.forEach((planet, index) => {
+      const apiSpecs = planet.querySelector(".planet--specs");
+      // Specific Specs Selectors
+      const apiDist = apiSpecs.querySelector(".dist");
+      const apiMass = apiSpecs.querySelector(".mass");
+      const apiTemp = apiSpecs.querySelector(".temp");
+      const apiDia = apiSpecs.querySelector(".dia");
+      const apiMoon = apiSpecs.querySelector(".moon");
+      const apiGravity = apiSpecs.querySelector(".gravity");
+      // ////////////////////////
+      // Dynamic Distance
+      const distP = apiDist.querySelector(".distP");
+      distP.innerHTML = `${(allBodies[index].semimajorAxis / 1000000).toFixed(
+        1
+      )}M km`;
+      // ////////////////////////
+      // Dynamic Mass
+      const massP = apiMass.querySelector(".massP");
+      massP.innerHTML = `${allBodies[index].mass.massValue}x10<sup>${allBodies[index].mass.massExponent}</sup>`;
+      // ///////////////////////
+      // Dynamic Temperature
+      const tempP = apiTemp.querySelector(".tempP");
+      tempP.innerHTML = `${(allBodies[index].avgTemp - 273.15).toFixed(1)} C`;
+      // //////////////////////
+      // Dynamic Diameter
+      const diaP = apiDia.querySelector(".diaP");
+      diaP.innerHTML = `${allBodies[index].equaRadius.toFixed(1)} km`;
+      // /////////////////////
+      // Dynamic Moons
+      const moonP = apiMoon.querySelector(".moonP");
+      moonP.innerHTML = `${
+        allBodies[index].moons === null ? 0 : allBodies[index].moons.length
+      }`;
+      // ////////////////////
+      // Dynamic Gravity
+      const gravP = apiGravity.querySelector(".gravP");
+      gravP.innerHTML = `${allBodies[index].gravity} m/s<sup>2</sup>`;
+      // ///////////////////
+    });
+  } catch (error) {
+    console.error(error);
+  }
+})();
 
+// Selects clicked dot and applies active class / Changes line width
 dots.forEach((dot, index) => {
   dot.addEventListener("click", function () {
     dots.forEach(function (dot) {
@@ -54,40 +108,86 @@ dots.forEach((dot, index) => {
     }
   });
 });
+// ////////
 
+// Resets clicked icons to default state when listing through nav
+function planetDefaultState() {
+  icons.forEach((icon) => icon.classList.remove("icon--hide"));
+  planetImgs.forEach((planet) => {
+    planet.classList.remove("shift--left");
+    planet.classList.remove("shift--right");
+  });
+  names.forEach((name) => name.classList.remove("hide"));
+  infos.forEach((info) => info.classList.remove("text--shift"));
+  specs.forEach((spec) => spec.classList.remove("spec--shift"));
+}
+// ////////
+
+// Applies active class to current planet container
 dots.forEach((dot, index) => {
   dot.addEventListener("click", function () {
-    planets.forEach(function (planet) {
-      planet.classList.remove("active--planet");
+    planets.forEach(function (planetCnt) {
+      planetCnt.classList.remove("active--planet");
     });
+    // Adding active class to planet container
     planets[index].classList.add("active--planet");
+    // //////
+    planetDefaultState();
   });
 });
+// ////////
 
-iconRight.addEventListener("click", function () {
-  planet.classList.toggle("shift--left");
-  planetInfo.classList.toggle("text--shift");
-  planetName.classList.toggle("hide");
+// Controlls planet elements dynamicaly according to current active class
+iconRight.addEventListener("click", rightClickHandler);
+
+function rightClickHandler() {
+  iconRight.classList.toggle("icon--fill");
+  const currentPlanetCnt = document.querySelector(".active--planet");
+  const currentPlanet = currentPlanetCnt.querySelector(".planet");
+  const currentInfo = currentPlanetCnt.querySelector(".planet--info");
+  const currentName = currentPlanetCnt.querySelector(".name");
+
+  currentPlanet.classList.toggle("shift--left");
+  currentInfo.classList.toggle("text--shift");
+  currentName.classList.toggle("hide");
   iconLeft.classList.toggle("icon--hide");
-});
+}
+// //////
 
-iconLeft.addEventListener("click", function () {
-  planet.classList.toggle("shift--right");
-  planetSpecs.classList.toggle("spec--shift");
-  planetName.classList.toggle("hide");
+// Controlls planet elements dynamicaly according to current active class
+iconLeft.addEventListener("click", leftClickHandler);
+
+function leftClickHandler() {
+  iconDot.classList.toggle("orbiting");
+  const currentPlanetCnt = document.querySelector(".active--planet");
+  const currentPlanet = currentPlanetCnt.querySelector(".planet");
+  const currentSpecs = currentPlanetCnt.querySelector(".planet--specs");
+  const currentName = currentPlanetCnt.querySelector(".name");
+
+  currentPlanet.classList.toggle("shift--right");
+  currentSpecs.classList.toggle("spec--shift");
+  currentSpecs.style.pointerEvents = "all";
+  currentName.classList.toggle("hide");
   iconRight.classList.toggle("icon--hide");
-});
+}
+// //////
 
-iconLeft.addEventListener("mouseenter", function () {
-  iconDot.classList.add("dot--size");
-});
-iconLeft.addEventListener("mouseleave", function () {
-  iconDot.classList.remove("dot--size");
-});
+// iconLeft.addEventListener("mouseenter", function () {
+//   iconDot.classList.add("dot--size");
+// });
+// iconLeft.addEventListener("mouseleave", function () {
+//   iconDot.classList.remove("dot--size");
+// });
 
 iconRight.addEventListener("mouseenter", function () {
   iconText.classList.add("text--bold");
 });
 iconRight.addEventListener("mouseleave", function () {
   iconText.classList.remove("text--bold");
+});
+
+specItems.forEach((spec) => {
+  spec.addEventListener("click", function () {
+    alert("clicked");
+  });
 });
